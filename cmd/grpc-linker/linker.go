4637b260d0198a774d00cd06888d8777cef31bd9
root <root@api-auth.etcchebao.com>
2019-04-02 17:15:21 +0800

    fixed gitignore

--- cmd/grpc-linker/linker.go
+++ cmd/grpc-linker/linker.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	_ "github.com/bullteam/zeus/pkg/routers"
-	"github.com/astaxie/beego"
 	//_ "github.com/astaxie/beego/cache/redis"
 	pb "github.com/bullteam/zeus/cmd/grpc-linker/grpc-server/proto"
 	"flag"
@@ -12,16 +10,16 @@ import (
 	"google.golang.org/grpc"
 )
 func main() {
-	pt := flag.String("p","8188","port of grpc server")
+	pt := flag.String("p", "8188", "port of grpc server")
 	flag.Parse()
-	lis, err := net.Listen("tcp", ":"+*pt)
+	lis, err := net.Listen("tcp", ":" + *pt)
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
-	}else{
-		log.Printf("Grpc serve on port:"+*pt)
+	} else {
+		log.Printf("Grpc serve on port:" + *pt)
 	}
 	s := grpc.NewServer()
-	pb.RegisterApiauthServer(s,&ApiAuthServer{})
+	pb.RegisterApiauthServer(s, &ApiAuthServer{})
 	//pb.RegisterCasbinServer(s, server.NewServer())
 	// Register reflection service on gRPC server.
 	reflection.Register(s)
@@ -29,5 +27,3 @@ func main() {
 		log.Fatalf("failed to serve: %v", err)
 	}
 }
-
-
