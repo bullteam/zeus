4e2ca10e9a1aba759c2915366e7030e81220f254
coso <wutongci@hotmail.com>
2019-04-09 12:02:14 +0800

    增加启动画面

--- cmd/api-server/server.go
+++ cmd/api-server/server.go
@@ -1,13 +1,27 @@
 package main
 
 import (
+	"fmt"
 	"github.com/astaxie/beego"
 	_ "github.com/bullteam/zeus/pkg/routers"
 )
 func main() {
+	usage()
 	if beego.BConfig.RunMode == "dev" {
 		beego.BConfig.WebConfig.DirectoryIndex = true
 		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
 	}
 	beego.Run()
 }
+
+func usage() {
+	fmt.Printf("%s\n", usageStr)
+}
+var usageStr = `
+  ______              
+ |___  /              
+    / / ___ _   _ ___ 
+   / / / _ \ | | / __|
+  / /_|  __/ |_| \__ \
+ /_____\___|\__,_|___/
+`
